712e913f94d1d00a8c812a8fbe931c7440b137af
Jon Dehdari <jon@dehdari.org>
2014-12-30 16:02:50 +0100

    clustercat.{c,h}: use word2class in init_clusters()

--- src/clustercat.c
+++ src/clustercat.c
@@ -100,7 +100,9 @@ int main(int argc, char **argv) {
 	char **unique_words = (char **)malloc(global_metadata.type_count * sizeof(char*));
 	get_keys(&ngram_map, unique_words);
 
-	init_clusters(cmd_args, global_metadata.type_count, unique_words);
+	word_id_t word2class[global_metadata.type_count];
+
+	init_clusters(cmd_args, global_metadata.type_count, word2class);
 	clock_t time_clusters_initialized = clock();
 	if (cmd_args.verbose > 0) {
 		fprintf(stderr, "%s: Finished initializing clusters in %.2f secs\n", argv_0_basename, (double)(time_clusters_initialized - time_model_built)/CLOCKS_PER_SEC);
@@ -124,7 +126,7 @@ int main(int argc, char **argv) {
 
 void get_usage_string(char * restrict usage_string, int usage_len) {
 
-	snprintf(usage_string, usage_len, "ClusterCat  (c) 2014 Jon Dehdari - LGPL v3 or Apache v2\n\
+	snprintf(usage_string, usage_len, "ClusterCat  (c) 2014 Jon Dehdari - LGPL v3 or Mozilla Public License v2\n\
 \n\
 Usage:    clustercat [options] < corpus.tok.txt > classes.tsv \n\
 \n\
@@ -431,7 +433,7 @@ void free_sent_info(struct_sent_info sent_info) {
 	free(sent_info.sent);
 }
 
-void init_clusters(const struct cmd_args cmd_args, unsigned long vocab_size, char **unique_words) {
+void init_clusters(const struct cmd_args cmd_args, word_id_t vocab_size, word_id_t word2class[restrict]) {
 	register unsigned long word_i = 0;
 
 	if (cmd_args.class_algo == EXCHANGE) { // It doesn't really matter how you initialize word classes in exchange algo.  This assigns words from the word list an incrementing class number from [0,num_classes].  So it's a simple pseudo-randomized initialization.
@@ -439,13 +441,13 @@ void init_clusters(const struct cmd_args cmd_args, unsigned long vocab_size, cha
 		for (; word_i < vocab_size; word_i++, class++) {
 			if (class > cmd_args.num_classes)
 				class = 1;
-			//printf("cls=%u, w=%s, w_i=%lu, vocab_size=%lu\n", class, unique_words[word_i], word_i, vocab_size);
-			map_update_class(&word2class_map, unique_words[word_i], class);
+			//printf("cls=%u, w_i=%lu, vocab_size=%lu\n", class, word_i, vocab_size);
+			word2class[word_i] = class;
 		}
 
 	} else if (cmd_args.class_algo == BROWN) { // Really simple initialization: one class per word
-		for (unsigned long class = 0; word_i < vocab_size; word_i++, class++)
-			map_update_class(&word2class_map, unique_words[word_i], class);
+		for (unsigned long class = 1; word_i < vocab_size; word_i++, class++)
+			word2class[word_i] = class;
 	}
 }
 
@@ -497,6 +499,7 @@ void cluster(const struct cmd_args cmd_args, char * restrict sent_store[const], 
 					end_cycle_short = false;
 
 					fprintf(stderr, " Moving '%s'\t%u -> %u\t(logprob %g -> %g)\n", word, old_class, best_hypothesis_class, log_probs[old_class-1], best_hypothesis_log_prob); fflush(stderr);
+					//word2class[word_i] = best_hypothesis_class;
 					map_update_class(&word2class_map, word, best_hypothesis_class);
 					best_log_prob = best_hypothesis_log_prob;
 				}
