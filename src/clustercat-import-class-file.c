dbe764aeec5bb49a8231179fb1a32650cebe5824
Jon Dehdari <jon@dehdari.org>
2015-01-20 21:20:49 +0100

    clustercat-import-class-file.c: do range checking on imported class file values

--- src/clustercat-import-class-file.c
+++ src/clustercat-import-class-file.c
@@ -4,7 +4,7 @@
 #include "clustercat-map.h"
 
 // Parse TSV file input and overwrite relevant word mappings
-void import_class_file(struct_map_word **word_map, word_id_t vocab_size, wclass_t word2class[restrict], const char * restrict class_file_name) {
+void import_class_file(struct_map_word **word_map, word_id_t vocab_size, wclass_t word2class[restrict], const char * restrict class_file_name, const wclass_t num_classes) {
 	char * restrict line_end;
 	char * restrict line = calloc(MAX_WORD_LEN + 9, 1);
 
@@ -23,6 +23,10 @@ void import_class_file(struct_map_word **word_map, word_id_t vocab_size, wclass_
 		line[keylen] = '\0'; // Split key and count
 		char * restrict key = line;
 		wclass_t class = atoi(line + keylen + 1);
+		if ((class < 1) || (class > num_classes)) {
+			fprintf(stderr,  " Error: Imported word classes from file \"%s\" must be in a range from 1-%u.  Word \"%s\" has class %i\n", class_file_name, num_classes, key, class); fflush(stderr);
+			exit(13);
+		}
 		//printf("keylen=%i, key=<<%s>>, class=<<%d>>\n", keylen, key, class);
 		word_id_t key_int = map_find_int(word_map, key);
 		word2class[key_int] = class;
