170d37989820e3a985570674d1609022e794fcb2
Jon Dehdari <jon@dehdari.org>
2015-02-05 17:56:45 +0100

    clustercat.c: parallelize bigram/rev-bigram building

--- src/clustercat.c
+++ src/clustercat.c
@@ -142,23 +142,36 @@ int main(int argc, char **argv) {
 	// Initialize and set word bigram listing
 	clock_t time_bigram_start = clock();
 	size_t bigram_memusage = 0; size_t bigram_rev_memusage = 0;
+	struct_word_bigram_entry * restrict word_bigrams = NULL;
+	struct_word_bigram_entry * restrict word_bigrams_rev = NULL;
 	if (cmd_args.verbose >= -1)
 		fprintf(stderr, "%s: Word bigram listing ... ", argv_0_basename); fflush(stderr);
-	struct_word_bigram_entry * restrict word_bigrams = calloc(global_metadata.type_count, sizeof(struct_word_bigram_entry));
+
+
+	#pragma omp parallel sections // Both bigram listing and reverse bigram listing can be done in parallel
+	{
+	#pragma omp section
+	{
+	word_bigrams = calloc(global_metadata.type_count, sizeof(struct_word_bigram_entry));
 	memusage += sizeof(struct_word_bigram_entry) * global_metadata.type_count;
 	bigram_memusage = set_bigram_counts(cmd_args, word_bigrams, sent_store_int, global_metadata.line_count, 0);
-	memusage += bigram_memusage;
+	}
 
 	// Initialize and set *reverse* word bigram listing
+	#pragma omp section
+	{
 	if (cmd_args.rev_alternate) { // Don't bother building this if it won't be used
-		struct_word_bigram_entry * restrict word_bigrams_rev = calloc(global_metadata.type_count, sizeof(struct_word_bigram_entry));
+		word_bigrams_rev = calloc(global_metadata.type_count, sizeof(struct_word_bigram_entry));
 		memusage += sizeof(struct_word_bigram_entry) * global_metadata.type_count;
 		bigram_rev_memusage = set_bigram_counts(cmd_args, word_bigrams_rev, sent_store_int, global_metadata.line_count, 1);
-		memusage += bigram_rev_memusage;
 	}
+	}
+	}
+
+	memusage += bigram_memusage + bigram_rev_memusage;
 	clock_t time_bigram_end = clock();
 	if (cmd_args.verbose >= -1)
-		fprintf(stderr, "in %'.2f secs.  Bigram memusage: %'.1f MB\n", (double)(time_bigram_end - time_bigram_start)/CLOCKS_PER_SEC, (bigram_memusage + bigram_rev_memusage)/(double)1048576); fflush(stderr);
+		fprintf(stderr, "in %'.2f CPU secs.  Bigram memusage: %'.1f MB\n", (double)(time_bigram_end - time_bigram_start)/CLOCKS_PER_SEC, (bigram_memusage + bigram_rev_memusage)/(double)1048576); fflush(stderr);
 
 	// Build <v,c> counts, which consists of a word followed by a given class
 	unsigned int * restrict word_class_counts = calloc(cmd_args.num_classes * global_metadata.type_count , sizeof(unsigned int));
