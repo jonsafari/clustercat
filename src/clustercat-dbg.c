ca7d7ca36551d16040e851c05def9c18d82132ad
Jon Dehdari <jon@dehdari.org>
2015-02-06 21:53:30 +0100

    m: clustercat-dbg.c: print_word_class_counts(): print count instead of offset

--- src/clustercat-dbg.c
+++ src/clustercat-dbg.c
@@ -2,9 +2,9 @@
 
 void print_word_class_counts(const struct cmd_args cmd_args, const struct_model_metadata model_metadata, const unsigned int * restrict word_class_counts) {
 	for (wclass_t class = 0; class < cmd_args.num_classes; class++) {
-		printf("Class=%u:\n\t", class);
+		printf("Class=%u   Offsets=%u,%u,...%u:\n\t", class, class, class+cmd_args.num_classes, (model_metadata.type_count-1) * cmd_args.num_classes + class);
 		for (word_id_t word = 0; word < model_metadata.type_count; word++) {
-			printf("#(<%u,%hu>)=%u  ", word, class, (word * cmd_args.num_classes + class));
+			printf("#(<%u,%hu>)=%u  ", word, class, word_class_counts[word * cmd_args.num_classes + class]);
 		}
 		printf("\n");
 	}
