7ffc747b7ff66dc264145e775dc53c53317d74b7
Jon Dehdari <jon@dehdari.org>
2016-04-05 14:08:39 +0200

    clustercat.py++

--- python/clustercat.py
+++ python/clustercat.py
@@ -3,9 +3,7 @@
 # MIT License
 # Simple Python wrapper for ClusterCat
 
-import sys
-import os.path
-import argparse
+import sys, argparse, subprocess
 parser = argparse.ArgumentParser(description='Converts words to integers, online')
 
 parser.add_argument('-i', '--in_map', help="Load pre-existing mapping file")
@@ -15,60 +13,69 @@ args = parser.parse_args()
 
 def cluster(text=None, infile=None, classes=None, class_file=None, class_offset=None, forward_lambda=None, ngram_input=None, min_count=None, out=None, print_freqs=None, quiet=None, refine=None, rev_alternate=None, threads=None, tune_cycles=None, unidirectional=None, verbose=None, word_vectors=None):
     clusters = {}
-    args = "clustercat "
+    cmd_str = "clustercat "
 
     if (infile):
-        args += " --in " + str(infile)
+        cmd_str += " --in " + str(infile)
 
     if (classes):
-        args += " --classes " + str(classes)
+        cmd_str += " --classes " + str(classes)
 
     if (class_file):
-        args += " --class-file " + str(class_file)
+        cmd_str += " --class-file " + str(class_file)
 
     if (class_offset):
-        args += " --class-offset " + str(class_offset)
+        cmd_str += " --class-offset " + str(class_offset)
 
     if (forward_lambda):
-        args += " --forward-lambda " + str(forward_lambda)
+        cmd_str += " --forward-lambda " + str(forward_lambda)
 
     if (ngram_input):
-        args += " --ngram-input "
+        cmd_str += " --ngram-input "
 
     if (min_count):
-        args += " --min-count " + str(min_count)
+        cmd_str += " --min-count " + str(min_count)
 
     if (out):
-        args += " --out " + out
+        cmd_str += " --out " + out
 
     if (print_freqs):
-        args += " --print-freqs "
+        cmd_str += " --print-freqs "
 
     if (quiet):
-        args += " --quiet "
+        cmd_str += " --quiet "
 
     if (refine):
-        args += " --refine " + str(refine)
+        cmd_str += " --refine " + str(refine)
 
     if (rev_alternate):
-        args += " --rev-alternate " + str(rev_alternate)
+        cmd_str += " --rev-alternate " + str(rev_alternate)
 
     if (threads):
-        args += " --threads " + str(threads)
+        cmd_str += " --threads " + str(threads)
 
     if (tune_cycles):
-        args += " --tune-cycles " + str(tune_cycles)
+        cmd_str += " --tune-cycles " + str(tune_cycles)
 
     if (unidirectional):
-        args += " --unidirectional "
+        cmd_str += " --unidirectional "
 
     if (verbose):
-        args += " --verbose "
+        cmd_str += " --verbose "
 
     if (word_vectors):
-        args += " --word-vectors " + word_vectors
+        cmd_str += " --word-vectors " + word_vectors
 
-    print(args, file=sys.stderr)
+    #print(cmd_str, file=sys.stderr)  # Use Python 3 interpreter
+
+    cmd_out = subprocess.check_output(cmd_str, shell=True, universal_newlines=True)
+
+    for line in cmd_out.split("\n"):
+        split_line = line.split("\t")
+        try:
+            clusters[split_line[0]] = split_line[1]
+        except:
+            pass
     return clusters
 
 def main():
